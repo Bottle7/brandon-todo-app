b149b6460e3f06de14449e8f39b565d5819e7347
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2023-04-07 17:05:07 -0700

    Frontend Todo Handlers
    Frontend handlers for actions done on todos.
    
    - handleChecked
    - handleUpdateTodo
    - handleCreateTodo
    - handleDeleteTodo
    - handleTodoComplete

---
+++ frontend/src/components/TodoList/handlers.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { BASE_URL } from '../../constants/variables';
+
+const handleUpdateTodo = (todo) => {
+  axios.patch(`${BASE_URL}/todos/${todo.id}`, todo);
+};
+
+const handleChecked = (checked, style) => {
+  if (checked) {
+    style.setProperty('font-style', 'italic');
+    style.setProperty('text-decoration', 'line-through');
+    style.setProperty('color', 'lightgrey');
+  } else {
+    style.removeProperty('font-style');
+    style.removeProperty('text-decoration');
+    style.removeProperty('color');
+  }
+};
+
+export const handleCreateTodo = (newTodo) => {
+  axios.post(`${BASE_URL}/todos`, newTodo);
+};
+
+export const handleDeleteTodo = (todoId) => {
+  axios.delete(`${BASE_URL}/todos/${todoId}`);
+};
+
+export const handleTodoComplete = (e, todo) => {
+  const labelStyle = e.target.labels[0].lastChild.style;
+  const targetChecked = e.target.checked;
+
+  todo.completed = e.target.checked;
+
+  handleChecked(targetChecked, labelStyle);
+  handleUpdateTodo(todo);
+};
